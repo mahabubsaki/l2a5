13a4483f53b08245a40a5d94f5ffffc95e21b418
mahabubsaki <md929820@gmail.com>
2024-02-13 05:33:18 +0600

    completed magnetic nav item

--- src/anim/Magnetic.tsx
+++ src/anim/Magnetic.tsx
@@ -1,42 +1,76 @@
+import classNames from 'classnames';
 import gsap from 'gsap';
-import { ReactNode, cloneElement, useEffect, useRef } from 'react';
+import { Dispatch, ReactNode, SetStateAction, cloneElement, useEffect, useRef } from 'react';
+import { NavPath } from '../components/Navbar';
 
-const Magnetic = ({ children }: { children: ReactNode; }) => {
-    const ref = useRef(null);
+
+const Magnetic = ({ children, pathname, path, hovered, setHovered }: { children: ReactNode; pathname: string; path: string; hovered: NavPath; setHovered: Dispatch<SetStateAction<NavPath>>; }) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+    const spanRef = useRef<HTMLSpanElement | null>(null);
+
+    const active = path === pathname;
+
+
+
     useEffect(() => {
+        const item = ref.current;
+        const spanItem = spanRef.current;
 
-        const item = ref.current ? (ref.current as HTMLElement) : null;
+
         const mouseMove = (e: MouseEvent) => {
-            if (item === null) return;
+
+            if (!item) return;
+
             const { clientX, clientY } = e;
             const { left, top, width, height } = item.getBoundingClientRect();
             const x = clientX - (left + width / 2);
             const y = clientY - (top + height / 2);
 
-            gsap.to(item, { x: x });
-            gsap.to(item, { y: y });
+            gsap.to(item, { x, y });
+
+            if (spanItem) {
+                gsap.to(spanItem, { x: x * 1.2, y: y * 1.2, scale: 1 });
+            }
         };
+
         const mouseLeave = () => {
-            gsap.to(item, { x: 0 });
-            gsap.to(item, { y: 0 });
+
+            if (!item) return;
+
+            gsap.to(item, { x: 0, y: 0 });
+
+            if (spanItem) {
+                gsap.to(spanItem, { x: 0, y: 0, scale: 0 });
+            }
         };
 
-        if (item === null) return;
+        if (item) {
+            item.addEventListener('mousemove', mouseMove);
+            item.addEventListener('mouseleave', mouseLeave);
 
-        item.addEventListener('mousemove', mouseMove);
-        item.addEventListener('mouseleave', mouseLeave);
+        }
+
         return () => {
-            if (item === null) return;
-            item.removeEventListener('mousemove', mouseMove);
-            item.removeEventListener('mouseleave', mouseLeave);
+            if (item) {
+                item.removeEventListener('mousemove', mouseMove);
+                item.removeEventListener('mouseleave', mouseLeave);
+            }
         };
-    }, [
+    }, []);
 
+    console.log({ active, hovered });
 
-
-    ]);
     return (
-        cloneElement(children, { ref })
+        <div className='relative' onMouseEnter={() => setHovered(path as NavPath)} onMouseLeave={() => setHovered(() => {
+            return null;
+        })}>
+            {cloneElement(children as React.ReactElement, { ref })}
+            <span ref={spanRef} className={classNames('size-[5px] left-1/2 rounded-full bg-black absolute right-1/2 bottom-1')} style={{ scale: 0 }} />
+
+            {(active && !hovered) ? <span className={classNames('size-[5px] left-1/2 rounded-full bg-black absolute right-1/2 bottom-1 duration-[5s]')} style={{ scale: (active && !hovered) ? 1 : 0 }} />
+                : null}
+
+        </div>
     );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Magnetic from '../anim/Magnetic';
 
 
@@ -8,20 +8,23 @@ const navItems = [
     { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
 
-];
-
+] as const;
+export type NavPath = typeof navItems[number]['path'] | null;
 
 const Navbar = () => {
+    const { pathname } = useLocation();
+    const [hovered, setHovered] = useState<NavPath>(null);
+    const navigate = useNavigate();
     return (
         <nav className='px-20 py-6 flex justify-between items-center'>
 
             <div>
                 <p className='text-3xl font-bold'>EVENT <span className='text-[#3461FF]'>360</span></p>
             </div>
-            <ul className='flex gap-4'>
+            <ul className='flex gap-8 items-center'>
                 {navItems.map((item, index) => (
-                    <Magnetic>
-                        <li key={index} className=' font-medium'>
+                    <Magnetic pathname={pathname} path={item.path} hovered={hovered} setHovered={setHovered}>
+                        <li onClick={() => navigate(item.path)} key={index} className='font-medium cursor-pointer select-none py-3 px-2'>
 
                             {item.name}
 
